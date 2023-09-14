71339a6130493f734e9c9d25f2d2ad241ad1cc66
sidney31 <surovbogdan05@gmail.com>
2023-09-14 22:34:18 +0300

    first commit

---
+++ aihouse/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-wg5dpmr7d)kqs6#j-3)*1h3b5du!#(f!iqy^&jl_n*9lwk)dml"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
