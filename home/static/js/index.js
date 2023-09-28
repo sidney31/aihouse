e9822fd3814ba9033e5544383bcae1d2dca06f85
sidney31 <surovbogdan05@gmail.com>
2023-09-28 14:16:20 +0300

    add gallery (while bad)

--- home/static/js/index.js
+++ home/static/js/index.js
@@ -1,13 +1,27 @@
+//header animate
 var navbar = document.querySelector(".navbar");
-
-console.log(navbar)
 
 if (navbar) {
     window.onwheel = e => {
-        if(e.deltaY > 0)
+        if (e.deltaY > 0)
             navbar.style.transform = "translateY(-100px)";
-        else 
+        else
             navbar.style.transform = "translateY(0)";
-        
+
     }
 }
+
+//gallery carousel
+var items = document.querySelectorAll(".gallery-carousel-inner .carousel-item");
+items.forEach((e) => {
+    const slide = 2;
+    let next = e.nextElementSibling;
+    for (let i = 0; i < slide; i++) {
+        if (!next) {
+            next = items[0];
+        }
+        let cloneChild = next.cloneNode(true);
+        e.appendChild(cloneChild.children[0]);
+        next = next.nextElementSibling;
+    }
+})
