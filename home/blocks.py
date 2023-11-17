fd68ca1f57ab3a122618269ea618b9a71e2feae6
sidney31 <surovbogdan05@gmail.com>
2023-11-17 21:10:52 +0300

    add inheritance in ButtonSnippets

---
+++ home/blocks.py
@@ -0,0 +1,43 @@
+from wagtail.blocks import(
+    CharBlock,
+    StructBlock,
+    RichTextBlock,
+)
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class TitleButtonBlock(StructBlock):
+    icon = CharBlock()
+    title = CharBlock()
+
+    class Meta:
+        template = 'blocks/title_button_block.html'
+
+
+class FunctionalDescBlock(StructBlock):
+    header = CharBlock()
+    description = CharBlock()
+
+    class Meta:
+        template = 'blocks/functional_desc_block.html'
+
+
+class CardCarouselBlock(StructBlock):
+    image = ImageChooserBlock()
+    title = CharBlock()
+    description = CharBlock()
+    hint = CharBlock()
+
+    class Meta:
+        template = 'blocks/card_carousel_block.html'
+
+
+class TitleAndSubtitle(StructBlock):
+    title = CharBlock()
+    subtitle = CharBlock()
+
+
+class Column(StructBlock):
+    text = RichTextBlock(
+        features=['small', 'medium', 'large']
+    )
---
+++ home/models.py
@@ -0,0 +1,153 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, PageChooserPanel
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.blocks import SnippetChooserBlock
+from wagtail import blocks
+
+from home.blocks import (
+    FunctionalDescBlock,
+    CardCarouselBlock,
+    TitleAndSubtitle,
+)
+
+
+class HomePage(Page):
+    subHeader = StreamField([
+        ("header", TitleAndSubtitle(label='Заголовок')),
+        ("button", SnippetChooserBlock("home.ButtonSnippet",
+                                       icon="th-list",
+                                       template="home/tags/button.html")
+         )
+    ],
+        block_counts={
+            'header': {'min_num': 1, 'max_num': 1},
+        },
+        null=False,
+        blank=False,
+        default='',
+        use_json_field=True)
+
+    title_buttons = StreamField([
+        ("button", SnippetChooserBlock("home.ButtonSnippet",
+                                       icon="th-list",
+                                       template="home/tags/button.html")
+         )
+    ],
+        null=False,
+        blank=False,
+        default='',
+        use_json_field=True)
+
+    functional_description = StreamField([
+        ("left_block", FunctionalDescBlock(
+            label="Правый блок",
+        )),
+        ("right_block", FunctionalDescBlock(
+            label="Левый блок",
+        )),
+        ('image', ImageChooserBlock()),
+    ],
+        block_counts={
+            'left_block': {'min_num': 2},
+            'right_block': {'min_num': 2},
+            'image': {'mix_num': 1, 'max_num': 1},
+        },
+
+        null=False,
+        blank=False,
+        default='',
+        use_json_field=True)
+
+    card_carousel = StreamField([
+        ("header", TitleAndSubtitle(
+            label='Заголовок'
+        )),
+        ("card", CardCarouselBlock(
+            label='Карточка'
+        )),
+    ],
+        null=False,
+        blank=False,
+        default='',
+        use_json_field=True)
+
+    contacts = StreamField([
+        ("column", blocks.RichTextBlock(
+            label='Столбец'
+        )),
+    ],
+        block_counts={
+            'column': {'mix_num': 3, 'max_num': 3},
+        },
+
+        null=False,
+        blank=False,
+        default='',
+        use_json_field=True)
+
+    ##########################################################################
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subHeader'),
+        FieldPanel('title_buttons'),
+        FieldPanel('functional_description'),
+        FieldPanel('card_carousel'),
+        FieldPanel('contacts'),
+    ]
+
+
+class BaseButtonSnippet(models.Model):
+    bodyText = models.TextField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.bodyText
+
+
+@register_snippet
+class ButtonSnippet(BaseButtonSnippet):
+    height = models.IntegerField()
+    width = models.IntegerField()
+    bodyText = models.TextField(null=True, blank=True)
+    url = models.URLField()
+    page = models.ForeignKey('wagtailcore.Page', blank=True, null=True, on_delete=models.SET_NULL)
+
+    panels = [
+        FieldPanel('height'),
+        FieldPanel('width'),
+        FieldPanel('bodyText'),
+        MultiFieldPanel([
+            FieldPanel('url'),
+            PageChooserPanel('page')
+        ],
+            heading='RedirectTo'
+        )
+    ]
+
+    def save(self, *args, **kwargs):
+        pass
+
+    class Meta:
+        verbose_name = 'Кнопка'
+        verbose_name_plural = 'Кнопки'
+
+
+@register_snippet
+class TextLinkSnippet(BaseButtonSnippet):
+    bodyText = models.TextField(null=True, blank=True)
+    url = models.URLField()
+
+    panels = [
+        FieldPanel('bodyText'),
+        FieldPanel('url'),
+    ]
+
+    class Meta:
+        verbose_name = 'Текст-ссылка'
+        verbose_name_plural = 'Текст-ссылки'
